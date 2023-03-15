edba2de61548f74ce78046aaaa5f57a9dd52d328
juribt1718 <juribt@rambler.ru>
2023-03-16 01:48:17 +0300

    some infa

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello new world!!");
+        System.out.println("Hello new wonderful world!!");
 	// write your code here
     }
 }
