97d81f719c709e67e36e12b8fe92a57a2f83c800
juribt1718 <juribt@rambler.ru>
2023-03-16 01:03:25 +0300

    message was changed

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        System.out.println("Hello new world!!");
 	// write your code here
     }
 }
