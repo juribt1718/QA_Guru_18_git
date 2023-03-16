2d701f6ae7e02de7cf60f854bc9fbf8d27949dcd
juribt1718 <125769166+juribt1718@users.noreply.github.com>
2023-03-17 01:14:34 +0300

    Update Main.java

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -1,9 +1,11 @@
 package guru.qa;
 
 public class Main {
+	
+	private static String msg = "Hello new world123!!!!!";
 
     public static void main(String[] args) {
-        System.out.println("Hello new world123!!!!!");
+        System.out.println(msg);
 	// write your code here
     }
 }
