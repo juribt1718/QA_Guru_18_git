c12a7500751aaebbe2b8ff53347dedc0889be191
juribt1718 <juribt@rambler.ru>
2023-03-17 01:12:38 +0300

    Added getMessage to the code

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,11 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello new world123!!");
+        System.out.println(getMessage());
 	// write your code here
+    }
+
+    private static String getMessage()
+    { return "Hello QA specialist!!";
     }
 }
